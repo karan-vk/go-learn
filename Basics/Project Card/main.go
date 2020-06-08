d13c345c261a13d95e632f29015b4d5b1d18f1ba
Karan V <karanvknarayanan@gmail.com>
2020-06-09 00:42:38 +0530

    write complete

--- Basics/Project Card/main.go
+++ Basics/Project Card/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	fmt.Println("*******************")
-	remainingCards.print()
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("*******************")
+	// remainingCards.print()
+	cards:=newDeck()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
