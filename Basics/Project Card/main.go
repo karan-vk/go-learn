4d41655089db813570bbbde71f03341d9d6be05b
Karan V <karanvknarayanan@gmail.com>
2020-06-08 23:13:47 +0530

    deal

--- Basics/Project Card/main.go
+++ Basics/Project Card/main.go
@@ -1,7 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
-
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("*******************")
+	remainingCards.print()
 }
